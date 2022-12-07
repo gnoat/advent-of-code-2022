b28c5a82e0995e1ecdc35ed6305198bb19b0a571
shane stephenson (shalst) <stephenson.shane.a@gmail.com>
2022-12-06 23:35:54 -0500

    added unit tests to day-3

--- day-3/rust/src/main.rs
+++ day-3/rust/src/main.rs
@@ -9,11 +9,25 @@ fn main() {
     // Create a hash map to measure priority for a given character
     let priority: HashMap<char, usize> = VALS.chars().enumerate().map(|(x, y)| (y, x)).collect();
 
+    let total_priority: usize = total_priority_calc(&DATA, &priority);
+    println!(
+        "The total priority of the mixed items is {}.",
+        total_priority
+    );
+
+    let total_group_priority: usize = total_group_priority_calc(&DATA, &priority);
+
+    println!(
+        "The total priority of each group's badges is {}.",
+        total_group_priority
+    );
+}
+
+fn total_priority_calc<'a>(DATA: &'a str, priority: &'a HashMap<char, usize>) -> usize {
     // Basically just iterate over the characters of each entry, split in two,
     // check for a common character, the use priority HashMap to evaluate each string
     // and sum together.
-    let total_priority: &usize = &DATA
-        .split("\n")
+    DATA.split("\n")
         .map(|row| {
             row[..row.len() / 2]
                 .chars()
@@ -23,18 +37,14 @@ fn main() {
                 .unwrap_or('0')
         })
         .map(|c| priority.get(&c).unwrap_or(&0))
-        .sum();
+        .sum()
+}
 
-    println!(
-        "The total priority of the mixed items is {}.",
-        total_priority
-    );
-
+fn total_group_priority_calc<'a>(DATA: &'a str, priority: &'a HashMap<char, usize>) -> usize {
     // Basically just chunks the data into three line groups and do the same as above,
     // except now we have to calculate the common character between three strings
     // instead of two.
-    let total_group_priority: usize = DATA
-        .split("\n")
+    DATA.split("\n")
         .collect::<Vec<&str>>()
         .chunks(3)
         .filter(|v| v.len() == 3)
@@ -47,10 +57,21 @@ fn main() {
                 .unwrap_or('0')
         })
         .map(|c| priority.get(&c).unwrap_or(&0))
-        .sum();
+        .sum()
+}
 
-    println!(
-        "The total priority of each group's badges is {}.",
-        total_group_priority
-    );
+#[test]
+fn example_tests() {
+    static TEST_DATA: &str = r#"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"#;
+
+    static VALS: &str = "0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let priority: HashMap<char, usize> = VALS.chars().enumerate().map(|(x, y)| (y, x)).collect();
+
+    assert_eq!(total_priority_calc(&TEST_DATA, &priority), 157);
+    assert_eq!(total_group_priority_calc(&TEST_DATA, &priority), 70);
 }
