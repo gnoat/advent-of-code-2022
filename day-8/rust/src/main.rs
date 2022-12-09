53d65e937ee71ac125dd0ddcb7ebf7109b8f108c
shane stephenson (shalst) <stephenson.shane.a@gmail.com>
2022-12-09 16:45:13 -0500

    cargo fmt

--- day-8/rust/src/main.rs
+++ day-8/rust/src/main.rs
@@ -1,26 +1,47 @@
-use std::include_str;
 use itertools::iproduct;
+use std::include_str;
 
 fn main() {
     static DATA: &str = include_str!("data.txt");
     let (width, height, trees, treepose) = read_string_to_vector(DATA);
 
-    println!("There are {} trees visible from outside the grid.", part_one(&trees, &width, &height));
-    println!("The highest scenic score possible is {}.", part_two(&trees, &treepose, &width, &height));
+    println!(
+        "There are {} trees visible from outside the grid.",
+        part_one(&trees, &width, &height)
+    );
+    println!(
+        "The highest scenic score possible is {}.",
+        part_two(&trees, &treepose, &width, &height)
+    );
 }
 
 fn part_one(trees: &Vec<Vec<usize>>, width: &usize, height: &usize) -> usize {
-    iproduct!(1..width-1, 1..height-1)
-        .filter(|&(x, y)| ((0..y).map(|i| trees[x][i]).max().unwrap_or(0) < trees[x][y]) ||
-                ((y+1..*height).map(|i| trees[x][i]).max().unwrap_or(0) < trees[x][y]) ||
-                ((0..x).map(|i| trees[i][y]).max().unwrap_or(0) < trees[x][y]) ||
-                ((x+1..*width).map(|i| trees[i][y]).max().unwrap_or(0) < trees[x][y]))
-        .count() + 2 * width + 2 * height - 4
+    iproduct!(1..width - 1, 1..height - 1)
+        .filter(|&(x, y)| {
+            ((0..y).map(|i| trees[x][i]).max().unwrap_or(0) < trees[x][y])
+                || ((y + 1..*height).map(|i| trees[x][i]).max().unwrap_or(0) < trees[x][y])
+                || ((0..x).map(|i| trees[i][y]).max().unwrap_or(0) < trees[x][y])
+                || ((x + 1..*width).map(|i| trees[i][y]).max().unwrap_or(0) < trees[x][y])
+        })
+        .count()
+        + 2 * width
+        + 2 * height
+        - 4
 }
 
-fn part_two(trees: &Vec<Vec<usize>>, treepose: &Vec<Vec<usize>>, width: &usize, height: &usize) -> usize {
-    iproduct!(1..width-1, 1..height-1)
-        .map(|(x, y)| [treewalker(&trees, &x, &y), treewalker(&treepose, &y, &x)].into_iter().flatten().product::<usize>())
+fn part_two(
+    trees: &Vec<Vec<usize>>,
+    treepose: &Vec<Vec<usize>>,
+    width: &usize,
+    height: &usize,
+) -> usize {
+    iproduct!(1..width - 1, 1..height - 1)
+        .map(|(x, y)| {
+            [treewalker(&trees, &x, &y), treewalker(&treepose, &y, &x)]
+                .into_iter()
+                .flatten()
+                .product::<usize>()
+        })
         .max()
         .unwrap()
 }
@@ -43,15 +64,22 @@ fn treewalker(data: &Vec<Vec<usize>>, x: &usize, y: &usize) -> [usize; 2] {
 }
 
 fn read_string_to_vector(data: &str) -> (usize, usize, Vec<Vec<usize>>, Vec<Vec<usize>>) {
-    let tree_array: Vec<Vec<usize>> = data.split("\n")
+    let tree_array: Vec<Vec<usize>> = data
+        .split("\n")
         .filter(|row| !row.is_empty())
-        .map(|row| row.chars()
-             .into_iter()
-             .map(|n| n.to_digit(10).unwrap() as usize)
-             .collect::<Vec<usize>>())
+        .map(|row| {
+            row.chars()
+                .into_iter()
+                .map(|n| n.to_digit(10).unwrap() as usize)
+                .collect::<Vec<usize>>()
+        })
         .collect();
     let transpose: Vec<Vec<usize>> = (0..tree_array.len())
-        .map(|y| (0..tree_array[0].len()).map(|x| tree_array[x][y]).collect::<Vec<usize>>())
+        .map(|y| {
+            (0..tree_array[0].len())
+                .map(|x| tree_array[x][y])
+                .collect::<Vec<usize>>()
+        })
         .collect();
     (tree_array[0].len(), tree_array.len(), tree_array, transpose)
 }
