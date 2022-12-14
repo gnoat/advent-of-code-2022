ab0065103caaf3029ae829f9c9c631e6533731cd
shane stephenson (shalst) <stephenson.shane.a@gmail.com>
2022-12-13 21:39:01 -0500

    updated day 12 visualization script to work on both windows and *nix systems

--- day-12/rust/view.py
+++ day-12/rust/view.py
@@ -1,17 +1,29 @@
-from os import system
+from os import system, name
 from time import sleep
 
+
+def clear():
+    if "nt" in name:
+        system("cls")
+    else:
+        system("clear")
+
+# Run the Rust solution and capture the stdout
 system("cargo run > motion.txt")
+
+# Read output from Rust solution
 with open("motion.txt", "r") as f:
     l = f.read()
+
+# Iterate over output from Rust solution and visualize
 for img in l.split("\n\n"):
     if "It takes" not in img:
-        system("clear")
+        clear()
         print(img)
         sleep(.025)
     else:
         print("\n[info]", img.strip())
         print(" - Press enter to proceed - ")
         input()
-        system("clear")
+        clear()
 system("rm motion.txt")
